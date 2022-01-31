c4847b93f94306fee03412bed528a9dcb33fd4e2
kratadata <hi@zybinska.io>
2022-01-31 16:25:14 +0100

    Update sprites.py

--- Sprite Animations/sprites.py
+++ Sprite Animations/sprites.py
@@ -1,9 +1,8 @@
 import pygame
+import os
 pygame.init()
 
-set_index = 0
-frame_set_start = set_index * 12 + 6
-frame_set_end = frame_set_start + 2
+
 
 speed = 10
 dwell = 1
@@ -14,9 +13,13 @@ third_character =  3
 fourth_character = 5
 
 direction = "idle"
+screen = pygame.display.set_mode((800,600))
 
 class AnimatedSprite:
-    def __init__(self, screen, x, y, frames):
+    # Javascript  constructor( screen, x, y, frames) {
+    #   this.screen = screen;
+    # }
+    def __init__(self, screen, x, y, frames): 
         self.screen = screen
         self.x = x
         self.y = y
@@ -25,13 +28,18 @@ class AnimatedSprite:
         self.rect = self.frames[self.index].get_rect()
         self.dwell_countdown = dwell
 
-    def advanceImage(self):
+    def advanceImage(self, index, frame_set_start, frame_set_end ):
+        
         self.dwell_countdown -= 1
+        
         if self.dwell_countdown < 0:
             self.dwell_countdown = dwell
-            self.index = (self.index+1)%(frame_set_end+1)
+            print(self.index)    
+            self.index = (self.index + 1)%(frame_set_end+1)
             if self.index<frame_set_start:
                 self.index = frame_set_start
+                
+                
 
     def draw(self):
         screen.blit(self.frames[self.index],
@@ -39,10 +47,9 @@ class AnimatedSprite:
                        int(self.y-self.rect.height/2) ))
 
 
-def nextFrame():
+def addFrames():
 
-    sheet = pygame.image.load('walkcycles.png')
-
+    sheet = pygame.image.load(os.path.join("Assets", 'walkcycles.png'))
     r = sheet.get_rect()
     rows = 8
     columns = 12
@@ -55,71 +62,77 @@ def nextFrame():
             rect = pygame.Rect(col*img_width, row*img_height, img_width, img_height)
             frames.append(sheet.subsurface(rect))
 
-
     return frames
 
+def main() :
+    clock = pygame.time.Clock()
 
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((800,600))
+    set_index = 0
+    frame_set_start = set_index * 12 + 6
+    frame_set_end = frame_set_start + 2
 
-frames = nextFrame()
+    frames = addFrames()
+ 
+    image_dims = frames[0].get_rect()
 
-image_dims = frames[0].get_rect()
+    #Reminder: this is how you resize the image if you want.
+    image_scale = 3
+    image_dims = (image_dims.w*image_scale,image_dims.h*image_scale)
 
-#Reminder: this is how you resize the image if you want.
-image_scale = 3
-image_dims = (image_dims.w*image_scale,image_dims.h*image_scale)
-for i in range(len(frames)):
-    frames[i] = pygame.transform.scale(frames[i], image_dims)
+    for i in range(len(frames)):
+        frames[i] = pygame.transform.scale(frames[i], image_dims)
 
-sprite = AnimatedSprite(screen, 400, 300, frames)
+    sprite = AnimatedSprite(screen, 400, 300, frames)
 
+   
 
+    #Draw all images on the screen
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    set_index = first_character
+                elif event.key == pygame.K_2:
+                    set_index = second_character
+                elif event.key == pygame.K_3:
+                    set_index = third_character
+                elif event.key == pygame.K_4:
+                    set_index = fourth_character
 
-#Draw all images on the screen
-done = False
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                set_index = first_character
-            elif event.key == pygame.K_2:
-                set_index = second_character
-            elif event.key == pygame.K_3:
-                set_index = third_character
-            elif event.key == pygame.K_4:
-                set_index = fourth_character
+            #Since set index has usually just been adjusted,
+            #update the frame starts and ends.
+        pressed = pygame.key.get_pressed()
 
-        #Since set index has usually just been adjusted,
-        #update the frame starts and ends.
-    pressed = pygame.key.get_pressed()
-
-    if pressed[pygame.K_LEFT]:
-        sprite.x-=speed
-        direction = "left"
-    elif pressed[pygame.K_RIGHT]:
-        sprite.x+=speed   
-        direction = "right"
-    else:
-        direction = "idle"
-    
-    if (direction == "left"):
-        frame_set_start = set_index * 12 + 9
-        frame_set_end = frame_set_start + 2
-    elif (direction == "right"):
-        frame_set_start = set_index * 12 + 3
-        frame_set_end = frame_set_start + 2
-    else:
-        frame_set_start = set_index * 12 + 6
-        frame_set_end = frame_set_start + 2
+        if pressed[pygame.K_LEFT]:
+            sprite.x-=speed
+            direction = "left"
+        elif pressed[pygame.K_RIGHT]:
+            sprite.x+=speed   
+            direction = "right"
+        else:
+            direction = "idle"
         
-    screen.fill((0,0,0)) #fill screen with black
-    sprite.draw()
-    sprite.advanceImage()
-    pygame.display.flip()
-    #Delay to get 30 fps
-    clock.tick(10)
-pygame.quit()
+        if (direction == "left"):
+            frame_set_start = set_index * 12 + 9
+            frame_set_end = frame_set_start + 2
+        elif (direction == "right"):
+            frame_set_start = set_index * 12 + 3
+            frame_set_end = frame_set_start + 2
+        else:
+            frame_set_start = set_index * 12 + 6
+            frame_set_end = frame_set_start + 2
+            
+        screen.fill((0,0,0)) #fill screen with black
+        sprite.draw()
+        sprite.advanceImage(set_index, frame_set_start, frame_set_end )
+        pygame.display.flip()
+
+        #Delay to get 30 fps
+        clock.tick(10)
+
+if __name__ == "__main__":
+    main()
